3c431036e63bd95567cb9eaea38933b1453bc20d
pengbo <tank99tank@qq.com>
2015-09-10 10:51:44 +0800

    首页布局

---
+++ src/main/java/com/fenghua/auto/webapp/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.fenghua.auto.webapp.controller;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping(value = "/")
+	public String index(HttpServletRequest request) throws Exception {
+
+		return "index";
+	}
+
+	public static void main(String[] agrs) {
+		System.out.println(new Date());
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+		System.out.println(new Date());
+	}
+}
