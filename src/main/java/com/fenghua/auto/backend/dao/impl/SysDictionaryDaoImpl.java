71eb80b86121588cd1aa6402b8514faaf8594c9e
pengbo <tank99tank@qq.com>
2015-09-10 15:54:16 +0800

    ddddd

--- src/main/java/com/fenghua/auto/backend/dao/impl/SysDictionaryDaoImpl.java
+++ src/main/java/com/fenghua/auto/backend/dao/impl/SysDictionaryDaoImpl.java
@@ -6,11 +6,14 @@ import com.fenghua.auto.backend.dao.SysDictionaryDao;
 import com.fenghua.auto.backend.domain.education.SysDictionary;
 
 /**
- * 字典数据库操作类接口实现类
- * @author LiuJunGuang
- * @date 2014年3月3日下午2:27:45
+ * 瀛楀吀鏁版嵁搴撴搷浣滅被鎺ュ彛瀹炵幇绫� * @author LiuJunGuang
+ * @date 2014骞�鏈�鏃ヤ笅鍗�:27:45
  */
 @Repository
 public class SysDictionaryDaoImpl extends BaseDaoImpl<SysDictionary> implements SysDictionaryDao {
 
+	
+	/**
+	 * Todo
+	 */
 }
