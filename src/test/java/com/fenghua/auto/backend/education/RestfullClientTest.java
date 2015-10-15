67d2d7b036bc0fe665af36fe53c5b6e1ab059660
shangyang <comedshang@163.com>
2015-10-15 13:00:41 +0600

    revert

--- src/test/java/com/fenghua/auto/backend/education/RestfullClientTest.java
+++ src/test/java/com/fenghua/auto/backend/education/RestfullClientTest.java
@@ -11,7 +11,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.ResponseEntity;
@@ -29,15 +28,28 @@ import com.fenghua.auto.backend.domain.education.Spittle;
  *
  * @version 
  *
- * @createTime锛�015骞�鏈�鏃�涓嬪崍4:35:57   
- *  
+ * @createTime：2015年9月9日 下午4:35:57 
+ *
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/spring-context.xml", "/spring-mybatis.xml"})
-@Ignore
 public class RestfullClientTest {
-	 
+	
+	/**
+	 * 
+	 * 该测试用例，要重点掌握的是，是如何通过不同的请求的类型，分发到不同的 SpittleRestfulController 对应的 GET 方法
+	 * 
+	 * 这里，调用的方法是
+	 * 
+		@RequestMapping(value = "/{id}", method = RequestMethod.GET, 
+						headers = {"Accept=text/xml, application/json"} ) // indicates, the server only accept the xml or json request.
+		public @ResponseBody Spittle getSpittle(@PathVariable("id") long id )
+		
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	@Test
 	public void getSpittleById_default() throws ClientProtocolException, IOException{
 		
@@ -45,11 +57,11 @@ public class RestfullClientTest {
 		
 		HttpClient httpClient = HttpClientBuilder.create().build(); // Notice: DefaultHttpClient is deprecated
 		
-		String url = "http://127.0.0.1:8080/spittle/" + spittleid; 
+		String url = "http://127.0.0.1:8080/spittle/" + spittleid;
 		
 		HttpGet getRequest = new HttpGet(url);
 		
-		getRequest.setHeader(new BasicHeader("Accept", "application/json")); // 鍙戦� json 璇锋眰
+		getRequest.setHeader(new BasicHeader("Accept", "application/json")); // 发送 json 请求
 		
 		HttpResponse response = httpClient.execute(getRequest);
 		
