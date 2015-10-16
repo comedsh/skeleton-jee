87ed2ab5ada690c9f21005372d826054234d5ff7
pengbo <tank99tank@qq.com>
2015-10-16 11:28:49 +0800

    修改代码

--- src/main/java/com/fenghua/auto/backend/dao/impl/SysDictionaryDaoImpl.java
+++ src/main/java/com/fenghua/auto/backend/dao/impl/SysDictionaryDaoImpl.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Repository;
 import com.fenghua.auto.backend.dao.SysDictionaryDao;
 import com.fenghua.auto.backend.domain.education.SysDictionary;
 
-/**
- * 瀛楀吀鏁版嵁搴撴搷浣滅被鎺ュ彛瀹炵幇绫� * @author LiuJunGuang
- * @date 2014骞�鏈�鏃ヤ笅鍗�:27:45
- */
 @Repository
 public class SysDictionaryDaoImpl extends BaseDaoImpl<SysDictionary> implements SysDictionaryDao {
 
