528470750b89b4e00cf8d0e3a7ee8f090c390df9
chengbin <bin.cheng@auto007.com>
2015-11-03 16:58:50 +0800

    用户注册包和类的申明

---
+++ src/main/java/com/fenghua/auto/backend/domain/register/Personal.java
@@ -0,0 +1,13 @@
+package com.fenghua.auto.backend.domain.register;
+
+import com.fenghua.auto.backend.domain.DomainObject;
+
+/**
+ * 个人实体类
+ * @author chengbin
+ * @createTime 2015.11.2
+ *
+ */
+public class Personal implements DomainObject {
+
+}
