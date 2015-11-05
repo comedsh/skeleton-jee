604e6801d91c16813ca186ed4d6795adeef66acb
chengbin <bin.cheng@auto007.com>
2015-11-05 10:17:59 +0800

    basedao的修改以及图形验证代码

--- src/main/java/com/fenghua/auto/backend/service/register/PersonalRegisServiceImpl.java
+++ src/main/java/com/fenghua/auto/backend/service/register/PersonalRegisServiceImpl.java
@@ -1,10 +1,48 @@
 package com.fenghua.auto.backend.service.register;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fenghua.auto.backend.dao.register.PersonalDao;
+import com.fenghua.auto.backend.domain.register.Personal;
+
 /**
  * 个人注册接口实现
  * @author chengbin
  * @createTime 2015.11.2
  *
  */
-public class PersonalRegisServiceImpl {
+@Service
+public class PersonalRegisServiceImpl implements PersonalRegisService {
+	
+	@Autowired
+	private PersonalDao personalDao;
 
+	@Override
+	public void delete(Long id) {
+		personalDao.deleteById(id);
+	}
+
+	@Override
+	public void update(Personal personal) {
+		personalDao.updateById(personal);
+	}
+
+	@Override
+	public void insert(Personal personal) {
+		personalDao.insert(personal);
+	}
+
+	@Override
+	public Personal getPersonalById(Long id) {
+		return personalDao.selectById(id);
+	}
+
+	@Override
+	public List<Personal> getAll() {
+		return personalDao.selectAll();
+	}
+	
 }
