066466e74185529558e1cd07becd5f1421afc538
chengbin <bin.cheng@auto007.com>
2015-11-05 10:24:30 +0800

    增加注释

--- src/main/java/com/fenghua/auto/backend/core/utills/graphValidate/VerifyCodeUtils.java
+++ src/main/java/com/fenghua/auto/backend/core/utills/graphValidate/VerifyCodeUtils.java
@@ -1,23 +1,25 @@
 package com.fenghua.auto.backend.core.utills.graphValidate;
 
-import java.awt.Color;  
-import java.awt.Font;  
-import java.awt.Graphics;  
-import java.awt.Graphics2D;  
-import java.awt.LinearGradientPaint;  
-import java.awt.Paint;  
-import java.awt.RenderingHints;  
-import java.awt.geom.AffineTransform;  
-import java.awt.image.BufferedImage;  
-import java.io.File;  
-import java.io.FileOutputStream;  
-import java.io.IOException;  
-import java.io.OutputStream;  
-import java.util.Arrays;  
-import java.util.Random;  
-  
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Random;
+
 import javax.imageio.ImageIO;  
-  
+ /**
+  *  图形验证类
+  * @author chengbin
+  *
+  */
 public class VerifyCodeUtils{  
   
     //使用到Algerian字体，系统里没有的话需要安装字体，字体只显示大写，去掉了1,0,i,o几个容易混淆的字符  
