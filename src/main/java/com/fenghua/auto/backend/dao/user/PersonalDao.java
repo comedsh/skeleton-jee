c56857f338c45cbd38b7b8f95118fd8aaeb88ad2
chengbin <bin.cheng@auto007.com>
2015-11-05 14:39:03 +0800

    修改目录结构

---
+++ src/main/java/com/fenghua/auto/backend/dao/user/PersonalDao.java
@@ -0,0 +1,16 @@
+package com.fenghua.auto.backend.dao.register;
+
+import org.springframework.stereotype.Repository;
+
+import com.fenghua.auto.backend.dao.BaseDao;
+import com.fenghua.auto.backend.domain.register.Personal;
+/**
+ * 个人注册dao
+ * @author chengbin
+ * @createtime 2015.11.2
+ *
+ */
+@Repository
+public interface PersonalDao extends BaseDao<Personal> {
+
+}
