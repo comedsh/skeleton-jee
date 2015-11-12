b0ddf9ac2c56960948e40dca172f4566e36e9a0e
chengbin <bin.cheng@auto007.com>
2015-11-12 17:07:08 +0800

    彭波 sqlID 修改以及相关文件的修改

--- src/main/java/com/fenghua/auto/backend/dao/constants/SqlId.java
+++ src/main/java/com/fenghua/auto/backend/dao/constants/SqlId.java
@@ -7,11 +7,11 @@ public interface SqlId {
 	//基本键
 	public String SQL_SELECT_COUNT = "selectCount";
 	public String SQL_SELECT = "select";
-	public String SQL_SELECT_BY_ID = "selectByPrimaryKey";
-	public String SQL_UPDATE_BY_ID = "updateByPrimaryKey";
-	public String SQL_UPDATE_BY_ID_SELECTIVE = "updateByPrimaryKeySelective";
+	public String SQL_SELECT_PRIMARY_KEY = "selectByPrimaryKey";
+	public String SQL_UPDATE_PRIMARY_KEY = "updateByPrimaryKey";
+	public String SQL_UPDATE_PRIMARY_KEY_SELECTIVE = "updateByPrimaryKeySelective";
 	public String SQL_DELETE = "delete";
-	public String SQL_DELETE_BY_ID = "deleteByPrimaryKey";
+	public String SQL_DELETE_PRIMARY_KEY = "deleteByPrimaryKey";
 	public String SQL_INSERT = "insert";
 	public String SQL_INSERT_SELECTIVE = "insertSelective";
 	
